7d0de1703300b387009419e7ce7d6ff06aae07e7
Daniel Monteiro <daniel.monteirofilho@outlook.com>
2021-08-10 16:46:04 -0300

    initial commit

---
+++ app/src/main/java/brcom/dan/example/portfoliopresenterapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package brcom.dan.example.portfoliopresenterapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import brcom.dan.example.portfoliopresenterapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
